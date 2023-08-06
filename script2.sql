05ee8533e2c018e3f6a3cf70846badf4d72119b4
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-06 12:58:21 +0530

    Day 1

---
+++ script2.sql
@@ -0,0 +1,28 @@
+INSERT INTO STUDENT VALUES(1 , 'NILESH' , 'ROY CHOWDHURY' , 'A+' , 'NIL@GMAIL.COM' , 'M');
+INSERT INTO STUDENT VALUES(2 , 'AGNIVA' , 'PAUL' , 'A' , 'AGNIVA@GMAIL.COM' , 'M');
+INSERT INTO STUDENT VALUES(3 , 'SHITIJ' , 'BHADRA' , 'A+' , 'SHITIJ@GMAIL.COM' , 'M');
+INSERT INTO STUDENT VALUES(4 , 'PRITAM' , 'SAHA' , 'A+' , 'PRITAM@GMAIL.COM' , 'M');
+INSERT INTO STUDENT VALUES(5 , 'ARGHADIP' , 'BANIK' , 'A+' , 'ARG@GMAIL.COM' , 'M');
+INSERT INTO STUDENT VALUES(6 , 'SENORITA' , 'ROY' , 'O' , 'SENO@GMAIL.COM' , 'F');
+
+
+INSERT INTO STUDENT VALUES(19 , 'ASH' , 'BABA' , 'AB+' , 'ASH@GMAIL.COM' , 'M');
+INSERT INTO STUDENT VALUES(20 , 'BROCK' , 'PAUL' , 'A' , 'BRR@GMAIL.COM' , 'M');
+INSERT INTO STUDENT VALUES(21 , 'MEOWTH' , 'ROCKET' , 'A+' , 'MEOW@GMAIL.COM' , '-');
+INSERT INTO STUDENT VALUES(22 , 'EMMA' , 'STONE' , 'A+' , 'PRITAM@GMAIL.COM' , 'F');
+INSERT INTO STUDENT VALUES(23 , 'NAAM NAHI' , 'PATA' , 'AB+' , 'NAAM@GMAIL.COM' , 'M');
+INSERT INTO STUDENT VALUES(24 , 'UNKOWN' , 'PERSON' , 'O-' , 'UNK@GMAIL.COM' , '-');
+
+insert into student values (7,'jeet','Bhadra','a+','jeet@gmail.com','m');
+  insert into student values (8,'selmon','bhai','z+','selmon@gmail.com','f');
+  insert into student values (9,'nilesha','roy','o+','nilesha@gmail.com','f');
+  insert into student values (10,'bhaat','daal','o-','bhaat@gmail.com','f');
+  insert into student values (11,'kacha','badam','b+','badam@gmail.com','m');
+  insert into student values (12,'goru','chhagol','z+','goru@gmail.com','n');
+  
+  insert into student values(13,'MOTA','Roy','B+','MOTA@gmail','M');
+insert into student values(14,'MODON','DA','B+','MO@gmail','M');
+insert into student values(15,'Pagla','Ro','B+','MOTA@gmai','M');
+insert into student values(16,'JIO','Roy','B+','MOA@gmail','M');
+insert into student values(17,'OTA','Roy','B+','OTA@gmail','F');
+insert into student values(18,'AGN','PAUL','B+','A@gmail','M');
